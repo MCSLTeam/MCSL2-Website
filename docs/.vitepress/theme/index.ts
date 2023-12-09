75239d490e4fe8821c1ca3d705bc25927f0e690b
LxHTT <lxhtt@vip.qq.com>
2023-12-09 13:07:39 +0800

    Fix | back to top

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,10 +5,8 @@ import "viewerjs/dist/viewer.min.css"
 import imageViewer from "vitepress-plugin-image-viewer"
 import { useRoute } from "vitepress"
 import "./style.css"
-import "vitepress-plugin-back-to-top/dist/style.css"
-import vitepressBackToTop from "vitepress-plugin-back-to-top"
-import "vitepress-markdown-timeline/dist/theme/index.css";
-
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+// import 'vitepress-plugin-back-to-top/dist/style.css'
 export default {
   ...Theme,
   enhanceApp(ctx) {
